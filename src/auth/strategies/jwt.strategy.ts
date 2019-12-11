33b4bb2fe8aff383ddc12e0d47e0a659e97f6ee0
Dennis Morent <dennismorent5@gmail.com>
2019-12-11 15:41:46 +0100

    implemented jwt strategy + user models

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,32 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { AuthService } from '../auth/auth.service';
+import { JwtPayload } from 'src/models/JwtPayload';
+
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+
+    constructor(private authService: AuthService){
+
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: 'secretkey'
+        });
+
+    }
+
+    async validate(payload: JwtPayload){
+
+        const user = await this.authService.validateUserByJwt(payload);
+
+        if(!user){
+            throw new UnauthorizedException();
+        }
+
+        return user;
+
+    }
+
+}
