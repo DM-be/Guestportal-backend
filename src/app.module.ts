28d67fdb158eb9e37a572fb93b5c897a5cab4e34
Dennis Morent <dennismorent5@gmail.com>
2019-12-12 13:41:40 +0100

    cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Pkcs11Service } from './services/pkcs11/pkcs11.service';
 import { EidGatewayModule } from './eid-gateway/eid-gateway.module';
-import { TestService } from './services/test/test.service';
 import { EidGateway } from './eid-gateway/eid-gateway.gateway';
 import { IseService } from './services/ise/ise.service';
 import { UsersModule } from './users/users.module';
@@ -13,6 +12,6 @@ import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [EidGatewayModule, UsersModule, MongooseModule.forRoot('mongodb://localhost/authexample'), AuthModule],
   controllers: [AppController],
-  providers: [AppService, Pkcs11Service, TestService, EidGateway, IseService, UsersModule, AuthModule],
+  providers: [AppService, Pkcs11Service, EidGateway, IseService, UsersModule, AuthModule],
 })
 export class AppModule {}
