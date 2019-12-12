21121cb1b8003f1d02961273f9ff476fa70d9c05
Dennis Morent <dennismorent5@gmail.com>
2019-12-12 13:26:49 +0100

    updated guest user models

--- src/models/CreateGuestUserDto.ts
+++ src/models/CreateGuestUserDto.ts
@@ -1,21 +1,9 @@
 import { GuestInfo } from './GuestInfo';
 import { GuestAccessInfo } from './GuestAccessInfo';
+import { GuestUser } from './GuestUser';
 
 export interface CreateGuestUserDto {
-  name: string; // resource name
-  id: string; // resource UUID --> "guestuserId" for deletebyid
-  description?: string;
-  customFields?: Object; // key value map
-  guestType?: string;
-  status?: string;
-  reasonForVisit?: string;
-  personBeingVisited?: string;
-  sponsorUsername?: string;
-  sponsorUserId?: string;
-  statusReason?: string;
-  portalId?: string; // needed to send email?
-  guestAccessInfo?: GuestAccessInfo;
-  guestInfo: GuestInfo;
+  GuestUser: GuestUser;
 }
 
 /*
---
+++ src/models/GuestUser.ts
@@ -0,0 +1,19 @@
+import { GuestAccessInfo } from "./GuestAccessInfo";
+import { GuestInfo } from "./GuestInfo";
+
+export interface GuestUser {
+    name: string; // resource name
+    id: string; // resource UUID --> "guestuserId" for deletebyid
+    description?: string;
+    customFields?: Object; // key value map
+    guestType?: string;
+    status?: string;
+    reasonForVisit?: string;
+    personBeingVisited?: string;
+    sponsorUsername?: string;
+    sponsorUserId?: string;
+    statusReason?: string;
+    portalId?: string; // needed to send email?
+    guestAccessInfo?: GuestAccessInfo;
+    guestInfo: GuestInfo;
+}
