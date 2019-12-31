ff255d0e45c7b8e656a96f79995325fb50a51b4c
Dennis Morent <dennismorent5@gmail.com>
2019-12-31 16:21:33 +0100

    active directory service documentation

--- src/services/active-directory/active-directory.service.ts
+++ src/services/active-directory/active-directory.service.ts
@@ -2,10 +2,29 @@ import { Injectable } from '@nestjs/common';
 import { ActiveDirectoryUser } from 'src/models/ActiveDirectoryUser';
 import { IseService } from '../ise/ise.service';
 
+/**
+ * microservice responsible for (indirect) communication with the Active Directory
+ *
+ * @export
+ * @class ActiveDirectoryService
+ */
 @Injectable()
 export class ActiveDirectoryService {
+  /**
+   *Creates an instance of ActiveDirectoryService.
+   * @param {IseService} iseService an instance of the IseService class (direct communication with ISE server)
+   * @memberof ActiveDirectoryService
+   */
   constructor(private iseService: IseService) {}
 
+  /**
+   * delegates a call to the IseService getActiveDirectoryUsers() to return AD users
+   *
+   * @returns {Promise< ActiveDirectoryUser[]>} 
+   * Promise containing all Active Directory Users
+   *  
+   * @memberof ActiveDirectoryService
+   */
   public async getListOfVisiteesFromActiveDirectory(): Promise<
     ActiveDirectoryUser[]
   > {
