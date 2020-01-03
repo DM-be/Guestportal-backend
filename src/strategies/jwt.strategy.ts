5318719ea7622cc1baf13e348275df50070e3cd6
Dennis Morent <dennismorent5@gmail.com>
2020-01-03 10:17:55 +0100

    strategy documentation

--- src/strategies/jwt.strategy.ts
+++ src/strategies/jwt.strategy.ts
@@ -3,30 +3,39 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { AuthService } from '../services/auth/auth.service';
 import { JwtPayload } from 'src/models/JwtPayload';
+import { TokenResponse } from 'src/models/TokenResponse';
 
-
+/**
+ * The default strategy used by the authguard decorators.
+ * Extracts the JWT token from the header and validates it using the authentication service
+ *
+ * @export
+ * @class JwtStrategy
+ * @extends {PassportStrategy(Strategy)}
+ */
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'secretkey',
+    });
+  }
 
-    constructor(private authService: AuthService){
-
-        super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: 'secretkey'
-        });
-
+  /**
+   * Validates a user given a given JWT payload.
+   * Returns a valid new TokenResponse
+   *
+   *
+   * @param {JwtPayload} payload
+   * @returns {Promise<TokenResponse>} Promise containing a valid JWT token
+   * @memberof JwtStrategy
+   */
+  async validate(payload: JwtPayload): Promise<TokenResponse> {
+    const signedResponse = await this.authService.validateUserByJwt(payload);
+    if (!signedResponse) {
+      throw new UnauthorizedException();
     }
-
-    async validate(payload: JwtPayload){
-
-        const user = await this.authService.validateUserByJwt(payload);
-
-        if(!user){
-            throw new UnauthorizedException();
-        }
-
-        return user;
-
-    }
-
+    return signedResponse;
+  }
 }
