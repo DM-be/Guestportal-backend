2587d54d8cc3413a1e0254fdbe989420c405a8c7
Dennis Morent <dennismorent5@gmail.com>
2020-01-03 10:18:41 +0100

    authentication and user service documentation + cleanup

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -1,68 +1,89 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/services/users/users.service';
-
 import { JwtPayload } from 'src/models/JwtPayload';
 import { TokenResponse } from 'src/models/TokenResponse';
 import { LoginUserDto } from 'src/models/LoginUserDto.dto';
+import { UserMongoose } from 'src/models/UserMongoose';
+import { Model } from 'mongoose';
+
+/**
+ * authentication service used to for user (admin) validation
+ * supports finding a user in the database and checking the encrypted password
+ * validates JWT tokens and issues new tokens when they are validated
+ *
+ * @export
+ * @class AuthService
+ */
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
-    constructor(private usersService: UsersService, private jwtService: JwtService){
-
-    }
-
-    async validateUserByPassword(loginAttempt: LoginUserDto): Promise<TokenResponse> {
-
-        // This will be used for the initial login
-        const userToAttempt = await this.usersService.findOneByEmail(loginAttempt.email);
-        
-        return new Promise((resolve) => {
-
-            // Check the supplied password against the hash stored for this email address
-            userToAttempt.checkPassword(loginAttempt.password, (err, isMatch) => {
-    
-                if(err) throw new UnauthorizedException();
-    
-                if(isMatch){
-                    // If there is a successful match, generate a JWT for the user
-                    resolve(this.createJwtPayload(userToAttempt));
-    
-                } else {
-                    throw new UnauthorizedException();
-                }
-    
-            });
-
-        });
-
-    }
-
-    async validateUserByJwt(payload: JwtPayload): Promise<TokenResponse> { 
-
-        // This will be used when the user has already logged in and has a JWT
-        let user = await this.usersService.findOneByEmail(payload.email);
-        if(user){
-            return this.createJwtPayload(user);
+  /**
+   * validates a user given his email and clear text password
+   * checks the hashed passwords and returns a valid token when validation is successfull
+   *
+   * @param {LoginUserDto} loginAttempt Email and password from the user
+   * @returns {Promise<TokenResponse>} Promise containing the token as a response
+   * @memberof AuthService
+   */
+  public async validateUserByPassword(
+    loginAttempt: LoginUserDto,
+  ): Promise<TokenResponse> {
+    const userToAttempt: Model<UserMongoose> = await this.usersService.findOneByEmail(
+      loginAttempt.email,
+    );
+    return new Promise(resolve => {
+      userToAttempt.checkPassword(loginAttempt.password, (err, isMatch) => {
+        if (err) throw new UnauthorizedException();
+        if (isMatch) {
+          resolve(this.createJwtPayload(userToAttempt));
         } else {
-            throw new UnauthorizedException();
+          throw new UnauthorizedException();
         }
+      });
+    });
+  }
 
+  /**
+   * Validates the user when he is already logged in with his JWT
+   *
+   * @param {JwtPayload} payload payload from within the JWT token
+   * @returns {Promise<TokenResponse>} a new signed token response
+   * @memberof AuthService
+   */
+  async validateUserByJwt(payload: JwtPayload): Promise<TokenResponse> {
+    const user: Model<UserMongoose> = await this.usersService.findOneByEmail(
+      payload.email,
+    );
+    if (user) {
+      return this.createJwtPayload(user);
+    } else {
+      throw new UnauthorizedException();
     }
+  }
 
-    private createJwtPayload(user): TokenResponse {
-
-        const data: JwtPayload = {
-            email: user.email
-        };
-        const jwt = this.jwtService.sign(data);
-        const tokenReponse: TokenResponse = {
-            expiresIn: 3600,
-            token: jwt
-        }
-        return tokenReponse;
-
-    }
-
+  /**
+   * creates a signed JWTPayload from a user model and returns this as a TokenResponse object
+   *
+   * @private
+   * @param {Model<UserMongoose>} userMongoose user model used by mongodb
+   * @returns {TokenResponse} response containing the signed JWT token
+   * @memberof AuthService
+   */
+  private createJwtPayload(userMongoose: Model<UserMongoose>): TokenResponse {
+    const data: JwtPayload = {
+      email: userMongoose.email,
+    };
+    const jwt = this.jwtService.sign(data);
+    const tokenReponse: TokenResponse = {
+      expiresIn: 3600,
+      token: jwt,
+    };
+    return tokenReponse;
+  }
 }
--- src/services/users/users.service.ts
+++ src/services/users/users.service.ts
@@ -4,21 +4,50 @@ import { UserMongoose } from 'src/models/UserMongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from 'src/models/CreateUserDto.dto';
 
+/**
+ * class used to create new administrator users
+ * saves the
+ *
+ * @export
+ * @class UsersService
+ */
 @Injectable()
 export class UsersService {
-    constructor(@InjectModel('User') private userModel: Model<UserMongoose>) {}
+  /**
+   *Creates an instance of UsersService.
+   * @param {Model<UserMongoose>} userModel the model used by mongodb
+   * @memberof UsersService
+   */
+  constructor(@InjectModel('User') private userModel: Model<UserMongoose>) {}
 
-    async create(createUserDto: CreateUserDto): Promise<void> {
-  
-      let createdUser  = new this.userModel(createUserDto);
+  /**
+   * creates a user and saves it in the user model
+   *
+   * @param {CreateUserDto} createUserDto create user DTO containing email and password
+   * @returns {Promise<void>}
+   * @memberof UsersService
+   */
+  public async create(createUserDto: CreateUserDto): Promise<void> {
+    try {
+      const createdUser = new this.userModel(createUserDto);
       return await createdUser.save();
-  
+    } catch (error) {
+      console.log(error);
     }
-  
-    async findOneByEmail(email: string): Promise<Model<UserMongoose>> {
-  
-      return await this.userModel.findOne({email: email});
-  
+  }
+
+  /**
+   * retrieves a user by email address (the unique identifier)
+   *
+   * @param {string} email
+   * @returns {Promise<Model<UserMongoose>>}
+   * @memberof UsersService
+   */
+  public async findOneByEmail(email: string): Promise<Model<UserMongoose>> {
+    try {
+      return await this.userModel.findOne({ email });
+    } catch (error) {
+      console.log(error);
     }
-  
+  }
 }
